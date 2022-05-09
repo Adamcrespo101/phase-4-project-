d175c39bb666f509ea9010cbe95dd3b6d6721ab0
Adam Crespo <alc49@LAPTOP-7KRCGAGF.localdomain>
2022-05-09 09:38:23 -0400

    more

--- client/src/components/Teacher.js
+++ client/src/components/Teacher.js
@@ -10,6 +10,13 @@ import Paper from '@mui/material/Paper';
 import Student from './Student';
 import GradeFilter from './GradeFilter'
 import { useState } from 'react';
+import Backdrop from '@mui/material/Backdrop';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Fade from '@mui/material/Fade';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
 
 function Teacher({teachers}){
 
@@ -18,6 +25,12 @@ function Teacher({teachers}){
     const [studentDisplay, setStudentDisplay] = useState(null) //studentDisplay is to grab the student object thats clicked on
 
     const [showForm, setShowForm]= useState(false) //show/hide new student form
+
+    const [open, setOpen] = React.useState(false);
+
+    const handleOpen = () => setOpen(true); //open handleOpen and handleClose- open and close the student detail modal
+
+    const handleClose = () => setOpen(false);
 
     const studentsArr = teachers.students //students for the logged in teacher
 
@@ -46,6 +59,18 @@ function Teacher({teachers}){
       function onlyUniqueCourses(value, index, self){
         return self.indexOf(value) === index;
       } 
+
+      const style = {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: 400,
+        bgcolor: 'background.paper',
+        border: '2px solid #000',
+        boxShadow: 24,
+        p: 4,
+      };
    
       const courseNames = gradesArr?.map((grade) => grade.course_name)
 
@@ -107,11 +132,31 @@ function Teacher({teachers}){
                   </div>
               </form>
             </div>
-            <div className='modal'>
-              <div className='modal-content'>
-                  <p>Name: {selectedStudent.name}</p>
-              </div>
-            </div>
+            
+              <div>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          <Box sx={style}>
+                  <Typography id="transition-modal-title" variant="h6" component="h2">
+                    Text in a modal
+                  </Typography>
+                    <Typography id="transition-modal-description" sx={{ mt: 2 }}>
+                      Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+                  </Typography>
+                </Box>
+            </Fade>
+          </Modal>
+        </div>
             <h2>{displayState ? "Students:" : "Grades:"}</h2> 
             <p className="show-button" onClick={changeDisplay}>{displayState? "Show Grades and Assignments" : "Return"}</p>
             <p className='show-button' onClick={handleShow}>{!showForm ? "Add Students to the roster" : "Close form"}</p>
