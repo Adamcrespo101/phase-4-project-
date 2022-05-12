2410242c2f1d8504a0baacef8c1e20dc731bb1a2
Brad Richardson <brichardson2468@gmail.com>
2022-05-12 09:54:28 -0400

    Deleted bootstrap, react-calendar

--- client/src/components/CalendarComponent.js
+++ client/src/components/CalendarComponent.js
@@ -1,24 +1,24 @@
-import React, { useState } from "react";
-import Calendar from "react-calendar/dist/umd/Calendar";
-import 'react-calendar/dist/Calendar.css';
+// import React, { useState } from "react";
+// import Calendar from "react-calendar/dist/umd/Calendar";
+// import 'react-calendar/dist/Calendar.css';
 
-function CalendarComponent() {
-  const [date, setDate] = useState(new Date());
+// function CalendarComponent() {
+//   const [date, setDate] = useState(new Date());
 
-  function handleUpdateDate(value) {
-    console.log("DATE: ", value.toISOString());
-    setDate(value);
-    setDate(value.toISOString());
-  }
+//   function handleUpdateDate(value) {
+//     console.log("DATE: ", value.toISOString());
+//     setDate(value);
+//     setDate(value.toISOString());
+//   }
 
-  return (
-      <div className="calendarContainer">
+//   return (
+//       <div className="calendarContainer">
 
-        <div className="innerCalendarContainer">
-          <Calendar onChange={handleUpdateDate} value={date} />
-        </div>
-      </div>
-  );
-}
+//         <div className="innerCalendarContainer">
+//           <Calendar onChange={handleUpdateDate} value={date} />
+//         </div>
+//       </div>
+//   );
+// }
 
-export default CalendarComponent;
+// export default CalendarComponent;
